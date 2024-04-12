2d025dfaed9d4f8f8b978f2a3aa0cc09d468573b
rambutan <pride.kraievskyi@gmail.com>
2024-04-12 17:25:59 +0300

    added pretier && db && prisma

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,9 @@
 export default function AuthLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return <div className="h-full flex items-center justify-center">{children}</div>;
+  return (
+    <div className="flex h-full items-center justify-center">{children}</div>
+  );
 }
